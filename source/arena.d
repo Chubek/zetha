854bc810f0f7dfd6de878106babf4c5b1f3ff0c0
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-12-29 22:02:59 +0330

    Cleaned, Fixed: the code was cleaned up, attribute-related errors removed ,casts fixed, conflicts removed

--- source/arena.d
+++ source/arena.d
@@ -1,6 +1,5 @@
 module zetha.arena;
 
-import core.stdc.stdlib : malloc, free;
 import core.stdc.string : memmove;
 import core.memory : GC;
 import core.lifetime : emplace;
@@ -8,16 +7,16 @@ import std.algorithm : max;
 
 import zetha.diag : getDiagEngine, Macros;
 
-enum GROWTH_RATE = 1.6;
+enum GROWTH_RATE = 2;
 enum INIT_SIZE = 2048;
 
 enum Lifetime
 {
     PERMANENT,
-    TRANSUNIT,
+    FILE,
     FUNCTION,
     SCOPE,
-    INTERN,
+    POOL,
 }
 
 struct Arena
@@ -27,7 +26,7 @@ struct Arena
     private size_t offset;
     private Lifetime lifetime;
 
-    this(size_t size, Lifetime)
+    this(size_t size = INIT_SIZE, Lifetime lifetime = Lifetime.SCOPE)
     {
         this.buffer = cast(ubyte*) GC.malloc(size, GC.BlkAttr.NO_SCAN);
 
@@ -36,6 +35,7 @@ struct Arena
 
         this.capacity = size;
         this.offset = 0;
+	this.lifetime = lifetime;
     }
 
     ~this()
@@ -43,17 +43,17 @@ struct Arena
         GC.free(this.buffer);
     }
 
-    @property size_t size() const pure nothrow @nogc
+    @property size_t size() const pure nothrow @nogc @safe
     {
         return this.capacity;
     }
 
-    @property Lifetime lifetime() const pure nothrow @nogc
+    @property Lifetime providence() const pure nothrow @nogc @safe
     {
         return this.lifetime;
     }
 
-    void* allocate(size_t reqSize, size_t reqAlign = (void*).alignof) nothrow @nogc
+    void* allocate(size_t reqSize, size_t reqAlign = (void*).alignof)
     {
         size_t aligned = (offset + reqAlign) & ~(reqAlign - 1);
 
@@ -65,18 +65,18 @@ struct Arena
         return mem;
     }
 
-    T* alloc(T, Args...)(size_t n, Args args) nothrow @nogc
+    T* alloc(T, Args...)(size_t n, Args args) g
     {
         auto mem = cast(T*) allocate(n * T.sizeof);
         return emplace!T(mem, args);
     }
 
-    void reset()
+    void reset() 
     {
         offset = 0;
     }
 
-    private void grow(size_t minBytes) @nogc
+    private void grow(size_t minBytes)
     {
         size_t newSize = capacity * GROWTH_RATE;
         while (newSize < offset + minBytes)
@@ -87,18 +87,11 @@ struct Arena
         if (newBuf is null)
             getDiagEngine().invokeMacro(Macros.MEMORY_ALLOCATION_FAILURE);
 
-        this.buffer = memmove(newBuf, this.buffer, newSize);
+        this.buffer = cast(ubyte*)memmove(newBuf, this.buffer, newSize);
 
         if (this.buffer is null)
             getDiagEngine().fatal("Memory reallocation failed").thenExit();
     }
 }
 
-shared static Arena gPermArena;
-shared static Arena gTUArena;
-shared static Arena gFuncArena;
-shared static Arena gScopeArena;
-shared static Arena gInternArena;
 
-// TODO: implement `shared static this()` for initialization
-// TODO: implement functions for allocating on each arena
