85b56ad80eb3b89ff989d23a21b064a44cc7fa28
Chubak Bidpaa <chubakbidpaa@riseup.net>
2026-01-03 22:59:51 +0330

    Init, Added: the package.d for this module

--- source/infra/package.d
+++ source/infra/package.d
@@ -3,5 +3,4 @@ module zetha.infra;
 public import zetha.infra.arena;
 public import zetha.infra.abstyy;
 public import zetha.infra.strpool;
-public import zetha.infra.arena;
-public import zetha.infra.provenance;
+public import zetha.infra.source;
